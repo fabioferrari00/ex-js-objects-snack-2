9d74e95143573b04404ca0388e0e7de92e6ce2c2
fabioferrari00 <fabioferarri.ff00@gmail.com>
2025-11-08 11:47:02 +0100

    snack 3

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log(secondBurger.name ); // Double Cheese Burger */
 
 //SNACK 2
 
-const hamburger = { 
+/* const hamburger = { 
 	name: "Cheese Burger", 
 	weight: 250,
 	ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
@@ -20,4 +20,26 @@ const secondBurger = {...hamburger};
 secondBurger.ingredients[0] = "Salad";
 
 console.log(hamburger.ingredients[0]); // Salad
-console.log(secondBurger.ingredients[0]); // Salad
+console.log(secondBurger.ingredients[0]); // Salad */
+
+//SNACK 3
+
+/* const hamburger = { 
+	name: "Cheese Burger", 
+	weight: 250,
+	maker: {
+		name: "Anonymous Chef",
+		restaurant: {
+			name: "Hyur's Burgers",
+			address: "Main Street, 123",
+			isOpen: true,
+		},
+		age: 29
+	}
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger); */
+
+//dato lo structuredClone, sono stati creati 3 oggetti ognuno con una sua struttura anche se complessa, 
+// l'unica cosa che non riesce a copiare, essendo una deep copy, sono le funzioni che in questo caso non abbiamo
